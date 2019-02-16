07ce5edaa3218a94958a523e871fbefc13c2b9eb
jdoneill <jdoneill@ucsc.edu>
2019-02-16 12:41:50 -0800

    added READ ME, added sprinting and walking funcionality, movement animations working

--- assignment5/Assets/playerMove.cs
+++ assignment5/Assets/playerMove.cs
@@ -10,11 +10,12 @@ public class playerMove : MonoBehaviour
     float speed;//speed of player
     float jump = 6;//height to jump on space
     private Vector3 move;
+    public Animator character;
 
     void Start()
     {//initialize stuff
         player = GetComponent<CharacterController>();
-        speed = 7;
+    //    speed = 7;
     }
 
     private void FixedUpdate()
@@ -22,17 +23,32 @@ public class playerMove : MonoBehaviour
 
         float yVel = move.y;//store y velocity for jump/gravity
 
-        if (player.isGrounded)//walk slow on ground
+        if (player.isGrounded && !Input.GetKey(KeyCode.LeftShift))//walk on ground
+        {
+            speed = 4;
+            character.SetBool("Grounded", true);
+            character.SetFloat("MoveSpeed", Input.GetAxis("Vertical")/2);//set anim to walk
+
+        }
+        else if (player.isGrounded && Input.GetKey(KeyCode.LeftShift))//run on ground
         {
             speed = 7;
+            character.SetBool("Grounded", true);
+            character.SetFloat("MoveSpeed", Input.GetAxis("Vertical"));//set anim to run
+
         }
         else if (!player.isGrounded)//walk slow on ground
         {
             speed = 4;
+            character.SetBool("Grounded", false);//play jump anims
         }
 
-        move = (transform.forward * Input.GetAxis("Vertical"))//Input.GetAxis("Vertical"))
-             + (transform.right * Input.GetAxis("Horizontal"));
+
+
+
+        move = (transform.forward * Input.GetAxis("Vertical")//) //tank controls for now
+             //+ (transform.right * Input.GetAxis("Horizontal")
+             );
 
         move = move.normalized * speed;// make it so you can't game the game
         move.y = yVel;// replace y velocity from earlier
@@ -40,6 +56,7 @@ public class playerMove : MonoBehaviour
         if (player.isGrounded && Input.GetKeyDown(KeyCode.Space))//press space to jump
         {
             move.y = jump;//hippity hop
+            character.SetTrigger("Jump");
         }
 
         move.y = move.y + (Physics.gravity.y * Time.deltaTime);
